5c635e862e88329d8ee57a18c328bd4f30d35469
Fellipe Rezende <felliperezende@iMac-de-Fellipe.local>
2020-03-26 12:34:17 -0300

    Created detail page styles

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,8 +1,67 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { Image, TouchableOpacity } from 'react-native';
 
-// import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
+
+import {
+  Container,
+  Header,
+  Incident,
+  IncidentText,
+  IncidentValue,
+  ContactBox,
+  HeroTitle,
+  HeroDescription,
+  Actions,
+  HandleButton,
+  HandleButtonText,
+} from './styles';
 
 export default function Detail() {
-  return <View />;
+  const navigation = useNavigation();
+
+  function navigateBack() {
+    navigation.goBack();
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Image source={logoImg} />
+        <TouchableOpacity onPress={navigateBack}>
+          <Feather name="arrow-left" size={28} color="#e02041" />
+        </TouchableOpacity>
+      </Header>
+
+      <Incident>
+        <IncidentText>ONG:</IncidentText>
+        <IncidentValue>APAE</IncidentValue>
+
+        <IncidentText>CASO:</IncidentText>
+        <IncidentValue>Cadelinha atropelada</IncidentValue>
+
+        <IncidentText>VALOR:</IncidentText>
+        <IncidentValue>R$ 120,00</IncidentValue>
+      </Incident>
+
+      <ContactBox>
+        <HeroTitle>Salve o dia!</HeroTitle>
+        <HeroTitle>Seja o herói desse caso.</HeroTitle>
+
+        <HeroDescription>Entre em contato</HeroDescription>
+
+        <Actions>
+          <HandleButton onPress={() => {}}>
+            <HandleButtonText>WhatsApp</HandleButtonText>
+          </HandleButton>
+
+          <HandleButton onPress={() => {}}>
+            <HandleButtonText>E-mail</HandleButtonText>
+          </HandleButton>
+        </Actions>
+      </ContactBox>
+    </Container>
+  );
 }
